31fd122cb149093e620b01c6df7ef23bcc66e14b
Jase DiNardo <jaseiscoding@gmail.com>
2020-08-04 11:31:33 -0700

    Solve bugs for game sequencer and update settl-it.html with tester
    Looking to have animation additions for game sequence for a more interactive feel

--- randomNum.js
+++ randomNum.js
@@ -10,96 +10,61 @@
   var ply2Name = document.querySelector('.name2-input');
   var wager = document.querySelector('.wager');
   var button = document.getElementById('btn');
-
+  var theNumber;
   /*----------------------------------------------*/
-  var reset = function() {
+  //          helper functions
 
+
+  var winner = function(scoreA, opponent) {
+    scoreA.style.textDecoration= "line-through"
+    scoreA.style.textDecorationColor= "red"
+    randomNum.textContent = opponent.value + ' has to ' + wager.value;
+  }
+  var reset = function(scoreA, scoreB) {
+    button.style.opacity = "100%"
+    button.textContent = "Play Again?"
+    randomNum.textContent = '';
+    scoreA.textContent = '';
+    scoreB.textContent = '';
+    scoreA.style.textDecoration = "none";
+    scoreB.style.textDecoration = "none";
   }
 
   /*----------------------------------------------*/
+  //              game sequence
 
   var magicNum = function() {
-    button.style.opacity = "0%"
-    score1.style.textDecoration= "none"
+    button.style.opacity = "0%";
     theNumber = Math.floor(Math.random() * (100 - 1) + 1);
     randomNum.textContent = theNumber;
-    //if player 1 wins
+    
+    if ((theNumber - parseFloat(ply1Num.value)) === (theNumber - parseFloat(ply2Num.value))) {
+      randomNum.textContent = 'You tied? Terrible luck please leave!';
+    }
+
     if (Math.abs(theNumber - parseFloat(ply1Num.value)) > Math.abs(theNumber - parseFloat(ply2Num.value))) {
-      score2.textContent += '|'
+      score2.textContent += '|';
       if (score2.textContent.length === 5) {
-        randomNum.textContent = ply1Name.value + ' has to ' + wager.value;
-        score2.style.textDecoration= "line-through"
-        score2.style.textDecorationColor= "red"
-        score1.textContent = '';
-        score2.textContent + ' ';
-        // randomNum.textContent = 'Play Again?'
+        winner(score2, ply1Name);
       } else if (score2.textContent.length === 6) {
-        button.style.opacity = "100%"
-        button.textContent = "Play Again?"
-        randomNum.textContent = '';
-        score2.textContent = '';
-        score1.textContent = '';
+        reset(score1, score2);
       }
-    } else if ((theNumber - parseFloat(ply1Num.value)) === (theNumber - parseFloat(ply2Num.value))) {
-      randomNum.textContent = 'You tied? Terrible luck please leave!'
     } else {
-      score1.textContent += '|'
+      score1.textContent += '|';
       if (score1.textContent.length === 5) {
-        randomNum.textContent = ply2Name.value + ' has to ' + wager.value;
-        score1.style.textDecoration= "line-through";
-        score1.style.textDecorationColor= "red";
-        score2.textContent = '';
-        score1.textContent + ' ';
-        // randomNum.textContent = 'Play Again?'
-        
+        winner(score1, ply2Name);
       } else if (score1.textContent.length === 6) {
-        button.style.opacity = "100%";
-        button.textContent = "Play Again?"
-        randomNum.textContent = '';
-        score2.textContent = '';
-        score1.textContent = '';
+        reset(score1, score2);
       }
-      
     }
   }
   var start = button.addEventListener('click', function() {
-    button.style.opacity = "0%"
+    button.style.opacity = "0%";
   })
   var click = clickBox.addEventListener('click', magicNum);
 
-
-  
-  // var resultWait;
-  // var wait = function() {
-  //   resultWait = window.setInterval(magicNum, 1000);
-  // }
-  // var show = function() {
-  //   window.clearTimeout(wait);
-  // }
-
-  // var click = clickBox.addEventListener('click', wait)
-  
-
-
-
-
-
-  // var windStop = function () {
-  //   window.clearInterval(windUp)
-  // }
-//   window.addEventListener('click', windStop);
-
-  
-  
-    //variables for log in page
-  // var wagerIn = document.getElementById('wager-input');
-  // var playerOne = {};
-  // var playerTwo = {};
-  // playerOne.name = document.getElementById('one-name').value;
-  // playerTwo.name = document.getElementById('two-name').value;
-  // playerOne.number = document.getElementById('number-one').value;
-  // playerTwo.number = document.getElementById('number-two').value;
-  // var cont = document.getElementById('continue');
+  /*--------------------------------------------------------*/
+  //                     Animations
  
 
 
